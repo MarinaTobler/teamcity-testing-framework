b9f1ae541cfac78fc34f9d7af36c8213b8246952
Marina Tobler <marina.tobler@wemakesoftware.eu>
2023-12-15 10:26:27 +0200

    ProjectCanBeCreatedByValidUserTest is updated

--- src/test/java/com/example/teamcity/api/CreateBuildConfigTests/BuildConfigCanBeCreatedTests.java
+++ src/test/java/com/example/teamcity/api/CreateBuildConfigTests/BuildConfigCanBeCreatedTests.java
@@ -3,9 +3,9 @@ package com.example.teamcity.api.CreateBuildConfigTests;
 import com.example.teamcity.api.BaseApiTest;
 import com.example.teamcity.api.generators.RandomData;
 import com.example.teamcity.api.models.BuildType;
-import com.example.teamcity.api.models.NewProjectDescription;
 import com.example.teamcity.api.requests.checked.CheckedBuildConfig;
 import com.example.teamcity.api.requests.checked.CheckedProject;
+import com.example.teamcity.api.requests.unchecked.UncheckedProject;
 import com.example.teamcity.api.spec.Specifications;
 import org.testng.annotations.Test;
 
@@ -16,27 +16,34 @@ public class BuildConfigCanBeCreatedTests extends BaseApiTest {
 
         var testData = testDataStorage.addTestData();
 
+        var projectDescription = testData.getProject();
+
+        // create project by superUser
         var project = new CheckedProject(Specifications.getSpec().superUserSpec())
-                .create(testData.getProject());
+                .create(projectDescription);
 
-        var newProjectDescription = NewProjectDescription
-                .builder()
-                .parentProject(project)
-                .name(RandomData.getValidName())
-                .id(RandomData.getValidID())
-                .copyAllAssociatedSettings(true)
-                .build();
 
-        var buildConfigId = RandomData.getString256();
+//        var buildConfigId = RandomData.getString256(); --> Error has occurred during request processing, status code:
+//        500 (Internal Server Error). Details: jetbrains.buildServer.serverSide.InvalidIdentifierException:
+//        Build configuration or template ID
+
+//        var buildConfigId = RandomData.getValidID();
+        var buildConfigId = testData.getBuildType().getId();
 
         var buildConfig = new CheckedBuildConfig(Specifications.getSpec().superUserSpec())
                 .create(BuildType.builder()
                         .id(buildConfigId)
                         .name(RandomData.getString80())
-                        .project(newProjectDescription)
+//                        .project(testData.getProject())
+                        .project(projectDescription)
                         .description(RandomData.getString226())
                         .build());
 
         softy.assertThat(buildConfig.getId()).isEqualTo(buildConfigId);
+
+//        new UncheckedProject(Specifications.getSpec().superUserSpec()).delete(project.getId());
+//      No need to delete project, as testData is deleting in AfterMethod in BaseApiTest - buildConfig should be created
+//      in same project from testData ??
+        new UncheckedProject(Specifications.getSpec().superUserSpec()).delete(projectDescription.getId());
     }
 }
--- src/test/java/com/example/teamcity/api/CreateProjectTests/ProjectCanBeCreatedByValidUserTest.java
+++ src/test/java/com/example/teamcity/api/CreateProjectTests/ProjectCanBeCreatedByValidUserTest.java
@@ -2,11 +2,8 @@ package com.example.teamcity.api.CreateProjectTests;
 
 import com.example.teamcity.api.BaseApiTest;
 import com.example.teamcity.api.generators.RandomData;
-import com.example.teamcity.api.models.NewProjectDescription;
-import com.example.teamcity.api.models.Project;
 import com.example.teamcity.api.requests.checked.CheckedProject;
 import com.example.teamcity.api.requests.checked.CheckedUser;
-import com.example.teamcity.api.requests.unchecked.UncheckedProject;
 import com.example.teamcity.api.spec.Specifications;
 import org.testng.annotations.Test;
 
@@ -17,25 +14,51 @@ public class ProjectCanBeCreatedByValidUserTest extends BaseApiTest {
         var testData = testDataStorage.addTestData();
 
         // Create valid user by superUser to create project by valid user
-        new CheckedUser(Specifications.getSpec().superUserSpec())
-                .create(testData.getUser());
+
+       var validUser = new CheckedUser(Specifications.getSpec().superUserSpec())
+              .create(testData.getUser());
+
+        var projectId = RandomData.getValidID();
 
         // Create project by valid user
+//        var project = new CheckedProject(Specifications.getSpec()
+//                .authSpec(testData.getUser()))
+//                .create(NewProjectDescription.builder()
+//                        .parentProject(Project.builder()
+//                                .locator("_Root")
+//                                .build())
+//                        .name(RandomData.getValidName())
+////                        .id(RandomData.getValidID())
+//                        .id(projectId)
+//                        .copyAllAssociatedSettings(true)
+//                        .build());
+
+
+
+//        var project = new CheckedProject(Specifications.getSpec()
+//                .authSpec(testData.getUser()))
+//                .create(NewProjectDescription.builder()
+//                        .parentProject(Project.builder()
+//                                .locator("_Root")
+//                                .build())
+//                        .name(testData.getProject().getName())
+//                        .id(testData.getProject().getId())
+//                        .copyAllAssociatedSettings(true)
+//                        .build());
+
+
         var project = new CheckedProject(Specifications.getSpec()
                 .authSpec(testData.getUser()))
-                .create(NewProjectDescription.builder()
-                        .parentProject(Project.builder()
-                                .locator("_Root")
-                                .build())
-                        .name(RandomData.getValidName())
-                        .id(RandomData.getValidID())
-                        .copyAllAssociatedSettings(true)
-                        .build());
+                .create(testData.getProject());
 
         // Check project is created
-        softy.assertThat(project.getId()).isEqualTo(RandomData.getValidID());
+//        softy.assertThat(project.getId()).isEqualTo(projectId);
+        softy.assertThat(project.getId()).isEqualTo(testData.getProject().getId());
 
-        // Delete created project by valid user
-        new UncheckedProject(Specifications.getSpec().authSpec(testData.getUser())).delete(RandomData.getValidID());
+
+        // No need to delete, as testData is deleting in AfterMethod in BaseApiTest!
+        // Delete created project by superUser, ad created Admin pwd and username are "admin", "admin", not that created
+        // new UncheckedProject(Specifications.getSpec().authSpec(validUser)).delete(project.getId());
+        // new UncheckedProject(Specifications.getSpec().superUserSpec()).delete(project.getId());
     }
 }
