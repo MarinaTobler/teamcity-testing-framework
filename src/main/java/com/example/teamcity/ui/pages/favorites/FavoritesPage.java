f15786ade07ca25266b91a42e8b71529b71379a5
Marina Tobler <marina.tobler@wemakesoftware.eu>
2024-02-25 20:40:34 +0200

    fixing error Unused import

--- src/main/java/com/example/teamcity/ui/pages/favorites/FavoritesPage.java
+++ src/main/java/com/example/teamcity/ui/pages/favorites/FavoritesPage.java
@@ -9,7 +9,7 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class FavoritesPage extends Page {
 //  *  private SelenideElement header = element(Selectors.byClass("ProjectPageHeader__title--ih"));
-    private SelenideElement header = $(byClassName("ProjectPageHeader__title--ih"));
+    private final SelenideElement header = $(byClassName("ProjectPageHeader__title--ih"));
 
     public final void waitUntilFavoritePageIsLoaded() {
         waitUntilPageIsLoaded();
