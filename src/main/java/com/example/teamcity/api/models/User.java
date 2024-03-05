06f08c4dbad6841876af4c83c15cc881329fc7d0
Marina Tobler <marina.tobler@wemakesoftware.eu>
2024-03-05 15:26:49 +0200

    fixing buildConfigurationTest: field id is added

--- src/main/java/com/example/teamcity/api/models/User.java
+++ src/main/java/com/example/teamcity/api/models/User.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+    private String id;
     private String username;
     private String password;
     private String email;
